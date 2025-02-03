3d05d5e22958bebe8dc41c15b60904ff448cb716
Nicolas JEANNE <jeanne.n@chu-toulouse.fr>
2025-02-03 20:20:36 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# rmsd-stability
-Compute the mean RMSD and the standard deviations.
+# RMSD stability
+
+From the RMSD computation CSV files of the rms script (https://github.com/njeanne/rms), compute the mean and 
+standard deviation of the RMSD values from a start frame to an end frame.
+A CSV file of the RMSD means and standard deviations for the selection and for the whole data is produced.
+
+## Conda environment
+
+A [conda](https://docs.conda.io/projects/conda/en/latest/index.html) YAML environment file is provided: `conda_env/rmsd_stability_env.yml`. The file contains all the dependencies to run the script.
+The conda environment is created using the command:
+```shell script
+# create the environment
+conda env create -f conda_env/rmsd_stability_env.yml
+
+# activate the environment
+conda activate rmsd_stability
+```
+
+## Usage
+
+```shell script
+./rmsd_stability.py --start 70000 --out results/first_frame_reference_RMSD_statistics.csv data
+```
+
+## Output
+
+A CSV file of the means and standard deviations for the selected frames,
+here from the 70 000th frame to the end, and from the first frame to the end:
+
+|sample                       |selected frames|RMSD mean selected frames|RMSD standard deviation selected frames|all frames|RMSD mean all frames|RMSD standard deviation all frames|
+|-----------------------------|---------------|-------------------------|---------------------------------------|----------|--------------------|----------------------------------|
+|AB248520-3e_WT_ORF1_0        |70000 - 255000 |15.39                    |0.33                                   |1 - 255000|14.89               |1.31                              |
+|AB291961-3f_WT_ORF1_0        |70000 - 255000 |16.99                    |0.2                                    |1 - 255000|16.76               |0.71                              |
+|AB437318-3b_WT_ORF1_0        |70000 - 255000 |17.58                    |0.33                                   |1 - 255000|17.5                |0.85                              |
+|EU495148-3f_WT_ORF1_0        |70000 - 255000 |16.09                    |0.36                                   |1 - 255000|15.9                |0.96                              |
+|FJ653660-3f_WT_ORF1_0        |70000 - 255000 |13.81                    |0.28                                   |1 - 255000|13.71               |0.66                              |
+|FJ956757-3f_WT_ORF1_0        |70000 - 255000 |14.97                    |0.35                                   |1 - 255000|14.53               |0.95                              |
+|HEPAC-100_GATM_ORF1_0        |70000 - 255000 |18.75                    |0.54                                   |1 - 255000|18.41               |1.48                              |
+|HEPAC-100_PEPB1_ORF1_0       |70000 - 255000 |16.37                    |0.21                                   |1 - 255000|15.96               |0.97                              |
+|HEPAC-12-1_duplication_ORF1_0|70000 - 255000 |13.12                    |0.62                                   |1 - 255000|12.31               |1.58                              |
+|HEPAC-12-2_duplication_ORF1_0|70000 - 255000 |21.58                    |0.36                                   |1 - 255000|20.98               |1.62                              |
+|HEPAC-12-3_duplication_ORF1_0|70000 - 255000 |14.14                    |0.43                                   |1 - 255000|14.03               |0.59                              |
+|HEPAC-154_KIF1B_ORF1_0       |70000 - 255000 |17.04                    |0.4                                    |1 - 255000|16.88               |0.6                               |
+|HEPAC-26_RPL6_ORF1_0         |70000 - 255000 |38.37                    |0.98                                   |1 - 255000|35.47               |6.0                               |
+|HEPAC-64_ZNF787_ORF1_0       |70000 - 255000 |14.91                    |0.31                                   |1 - 255000|14.7                |0.76                              |
+|HEPAC-6_RNF19A_ORF1_0        |70000 - 255000 |18.93                    |0.5                                    |1 - 255000|18.16               |1.49                              |
+|HEPAC-93_EEF1A1_ORF1_0       |70000 - 255000 |21.51                    |0.21                                   |1 - 255000|21.31               |0.96                              |
+|HEPAC-93_RNA18SP5_ORF1_0     |70000 - 255000 |16.47                    |0.23                                   |1 - 255000|16.19               |0.77                              |
+|HEPAC-95_duplication_ORF1_0  |70000 - 255000 |18.67                    |0.31                                   |1 - 255000|18.3                |1.0                               |
+|JN837481-3a_WT_ORF1_0        |70000 - 255000 |10.28                    |0.18                                   |1 - 255000|10.23               |0.36                              |
+|JN906974-3f_WT_ORF1_0        |70000 - 255000 |17.42                    |0.27                                   |1 - 255000|16.87               |1.16                              |
+|JQ679013_RPS17_ORF1_0        |70000 - 255000 |14.41                    |0.21                                   |1 - 255000|13.82               |1.23                              |
+|JQ679014_WT_ORF1_0           |70000 - 255000 |18.1                     |0.27                                   |1 - 255000|17.53               |1.51                              |
+|KT447527-3efg_WT_ORF1_0      |70000 - 255000 |12.24                    |0.31                                   |1 - 255000|12.06               |0.74                              |
+|KT447528-3a_WT_ORF1_0        |70000 - 255000 |14.75                    |0.25                                   |1 - 255000|14.74               |0.5                               |
+|KU980235-3f_WT_ORF1_0        |70000 - 255000 |12.02                    |0.22                                   |1 - 255000|11.9                |0.55                              |
+|KY232312-3f_WT_ORF1_0        |70000 - 255000 |17.81                    |0.43                                   |1 - 255000|17.71               |0.74                              |
+|KY780957-3chi_WT_ORF1_0      |70000 - 255000 |12.47                    |0.78                                   |1 - 255000|11.98               |1.17                              |
+|MF444031-3c_WT_ORF1_0        |70000 - 255000 |11.22                    |0.97                                   |1 - 255000|11.49               |1.14                              |
+|MG783569-3_WT_ORF1_0         |70000 - 255000 |15.56                    |0.22                                   |1 - 255000|14.96               |1.25                              |
---
+++ rmsd_stability.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+
+"""
+Created on 09 Dec. 2022
+"""
+
+__author__ = "Nicolas JEANNE"
+__copyright__ = "GNU General Public License"
+__email__ = "jeanne.n@chu-toulouse.fr"
+__version__ = "1.0.0"
+
+import argparse
+import logging
+import os
+import re
+import statistics
+import sys
+
+import pandas as pd
+
+
+def create_log(path, level):
+    """Create the log as a text file and as a stream.
+
+    :param path: the path of the log.
+    :type path: str
+    :param level: the level og the log.
+    :type level: str
+    :return: the logging:
+    :rtype: logging
+    """
+
+    log_level_dict = {"DEBUG": logging.DEBUG,
+                      "INFO": logging.INFO,
+                      "WARNING": logging.WARNING,
+                      "ERROR": logging.ERROR,
+                      "CRITICAL": logging.CRITICAL}
+
+    if level is None:
+        log_level = log_level_dict["INFO"]
+    else:
+        log_level = log_level_dict[level]
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    logging.basicConfig(format="%(asctime)s %(levelname)s:\t%(message)s",
+                        datefmt="%Y/%m/%d %H:%M:%S",
+                        level=log_level,
+                        handlers=[logging.FileHandler(path), logging.StreamHandler()])
+    return logging
+
+
+if __name__ == "__main__":
+    descr = f"""
+    {os.path.basename(__file__)} v. {__version__}
+
+    Created by {__author__}.
+    Contact: {__email__}
+    {__copyright__}
+
+    Distributed on an "AS IS" basis without warranties or conditions of any kind, either express or implied.
+
+    From the RMSD computation CSV files of the rms script (https://github.com/njeanne/rms), compute the mean and 
+    standard deviation of the RMSD values from a start frame to an end frame.
+    A CSV file of the RMSD means and standard deviations for the selection and for the whole data is produced.
+    """
+    parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-o", "--out", required=True, type=str, help="the path to the output CSV file.")
+    parser.add_argument("-s", "--start", required=False, default=1, type=int,
+                        help="the start frame (1-index) to consider in the \"frames\" column. Default is one.")
+    parser.add_argument("-e", "--end", required=False, type=int,
+                        help="the end frame (1-index) to consider in the \"frames\" column. If not used the last frame "
+                             "of the RMSD file is selected.")
+    parser.add_argument("-l", "--log", required=False, type=str,
+                        help="the path for the log file. If this option is skipped, the log file is created in the "
+                             "output directory.")
+    parser.add_argument("--log-level", required=False, type=str,
+                        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+                        help="set the log level. If the option is skipped, log level is INFO.")
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("input", type=str, help="the path of the RMSD CSV file directory.")
+    args = parser.parse_args()
+
+    # create output directory if necessary
+    output_directory = os.path.dirname(args.out)
+    os.makedirs(output_directory, exist_ok=True)
+    # create the logger
+    if args.log:
+        log_path = args.log
+    else:
+        log_path = os.path.join(output_directory, f"{os.path.splitext(os.path.basename(__file__))[0]}.log")
+    create_log(log_path, args.log_level)
+
+    logging.info(f"version: {__version__}")
+    logging.info(f"CMD: {' '.join(sys.argv)}")
+    if not args.end:
+        logging.info(f"no --end option used, last frame will be selected.")
+    
+    data_stats = {"sample": [], "selected frames": [], "RMSD mean selected frames (\u212B)": [],
+                  "RMSD standard deviation selected frames (\u212B)": [], "all frames": [],
+                  "RMSD mean all frames (\u212B)": [],
+                  "RMSD standard deviation all frames (\u212B)": []}
+    pattern_sample = re.compile("RMSD_(.+)_ORF1.csv")
+
+    for rmsd_csv in sorted(os.listdir(args.input)):
+        df_rmsd = pd.read_csv(os.path.join(args.input, rmsd_csv), sep=",")
+        last_frame = df_rmsd["frames"].iloc[-1] + 1
+        if args.end:
+            if args.end >= args.start:
+                try:
+                    raise argparse.ArgumentTypeError(f"--end {args.end} is greater or equal to --start {args.start}.")
+                except argparse.ArgumentTypeError as ex:
+                    logging.error(ex, exc_info=True)
+                    sys.exit(1)
+            try:
+                if args.end > last_frame:
+                    raise argparse.ArgumentTypeError(f"--end {args.end} is greater than the last frame of the input "
+                                                     f"RMSD computation file for {rmsd_csv}: {last_frame}")
+            except argparse.ArgumentTypeError as ex:
+                logging.error(ex, exc_info=True)
+                sys.exit(1)
+            end = args.end
+        else:
+            end = last_frame
+
+        # compute the stats for the whole data:
+        sample = pattern_sample.match(rmsd_csv).group(1)
+        if not sample:
+            logging.error(f"the sample pattern \"{pattern_sample.pattern}\" do not match with the file {rmsd_csv}.")
+            sys.exit(1)
+        data_stats["sample"].append(sample)
+        logging.info(f"{sample}:")
+        all_frames = f"1 - {last_frame}"
+        all_mean = round(statistics.mean(df_rmsd["RMSD"]), 2)
+        all_standard_deviation = round(statistics.stdev(df_rmsd["RMSD"]), 2)
+        data_stats["all frames"].append(all_frames)
+        data_stats["RMSD mean all frames (\u212B)"].append(all_mean)
+        data_stats["RMSD standard deviation all frames (\u212B)"].append(all_standard_deviation)
+        logging.info(f"\tframes {all_frames}:")
+        logging.info(f"\t\tRMSD mean:\t{all_mean} \u212B")
+        logging.info(f"\t\tRMSD sd:\t{all_standard_deviation} \u212B")
+
+        # compute the stats for the selection
+        selection_frames = f"{args.start} - {end}"
+        data_stats["selected frames"].append(selection_frames)
+        idx_start = int(df_rmsd[df_rmsd["frames"] == args.start - 1].index[0])
+        idx_end = int(df_rmsd[df_rmsd["frames"] == end - 1].index[0])
+        subset = df_rmsd.iloc[idx_start:idx_end+1]
+        selection_mean = round(statistics.mean(subset["RMSD"]), 2)
+        selection_standard_deviation = round(statistics.stdev(subset["RMSD"]),2)
+        data_stats["RMSD mean selected frames (\u212B)"].append(selection_mean)
+        data_stats["RMSD standard deviation selected frames (\u212B)"].append(selection_standard_deviation)
+        logging.info(f"\tframes {selection_frames}:")
+        logging.info(f"\t\tRMSD mean:\t{selection_mean} \u212B")
+        logging.info(f"\t\tRMSD sd:\t{selection_standard_deviation} \u212B")
+
+    df_stats = pd.DataFrame.from_dict(data_stats)
+    df_stats.to_csv(args.out, index=False, sep=";")
+    logging.info(f"result file: {args.out}")
